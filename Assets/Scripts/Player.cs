679fbf030a83bfdd8f36f8aaea1b662f58c729fd
vargas-g <vargasg3@uci.edu>
2025-01-10 16:39:58 -0800

    implemented player movement with WASD keys

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,30 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        // player moves up if holds or taps the "W" key.
+        PlantSeed();
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+
+        // player moves right if holds or taps the "D" key.
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+
+        // player moves down if holds or taps the "S" key.
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+
+        // player moves left if holds or taps the "A" key.
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
